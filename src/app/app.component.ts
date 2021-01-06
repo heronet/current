8b90b550c71afa03b79118788062f082bb46832c
HanDecoder <sirat4757@gmail.com>
2021-01-06 17:52:12 +0600

    added features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,23 @@ import { Currency } from './models/currency';
 })
 export class AppComponent implements OnInit {
   CURRENCY_API_KEY: string = "009847a6f613b87f484d";
-  available_currencies: string[] = [];
+  available_currencies: Currency;
   converted_amount: string;
   converted_cur: string;
+  converted_symbol: string;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.http.get(`https://free.currconv.com/api/v7/currencies?apiKey=${this.CURRENCY_API_KEY}`).subscribe((res: Currency) => {
-      for(const code in res.results) {
-        this.available_currencies.push(code);
-      }
-      this.available_currencies.sort();
+      this.available_currencies = res;
     });
   }
   onClickConvert(selected_cur: string, converted_cur: string, amount: number) {
     this.http.get(`https://free.currconv.com/api/v7/convert?apiKey=${this.CURRENCY_API_KEY}&q=${selected_cur}_${converted_cur}&compact=ultra`).subscribe((res: {converted: string, amount: number}) => {
       this.converted_amount = (amount * (res[Object.keys(res)[0]])).toString();
       this.converted_cur = converted_cur;
+      this.converted_symbol = this.available_currencies.results[converted_cur].currencySymbol;
     })
   }
 }

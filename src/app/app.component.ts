24745108e4ed4d698745811cdd4978641126e7f8
heronet <sirat4757@gmail.com>
2022-12-26 03:29:42 +0600

    changed api key

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,10 @@ import { Currency } from './models/currency';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  CURRENCY_API_KEY: string = "009847a6f613b87f484d";
+  CURRENCY_API_KEY: string = 'cff04706919575ebb9cd';
   available_currencies: Currency;
   converted_amount: string;
   converted_cur: string;
@@ -17,15 +17,24 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get(`https://free.currconv.com/api/v7/currencies?apiKey=${this.CURRENCY_API_KEY}`).subscribe((res: Currency) => {
-      this.available_currencies = res;
-    });
+    this.http
+      .get(
+        `https://free.currconv.com/api/v7/currencies?apiKey=${this.CURRENCY_API_KEY}`
+      )
+      .subscribe((res: Currency) => {
+        this.available_currencies = res;
+      });
   }
   onClickConvert(selected_cur: string, converted_cur: string, amount: number) {
-    this.http.get(`https://free.currconv.com/api/v7/convert?apiKey=${this.CURRENCY_API_KEY}&q=${selected_cur}_${converted_cur}&compact=ultra`).subscribe((res: {converted: string, amount: number}) => {
-      this.converted_amount = (amount * (res[Object.keys(res)[0]])).toString();
-      this.converted_cur = converted_cur;
-      this.converted_symbol = this.available_currencies.results[converted_cur].currencySymbol;
-    })
+    this.http
+      .get(
+        `https://free.currconv.com/api/v7/convert?apiKey=${this.CURRENCY_API_KEY}&q=${selected_cur}_${converted_cur}&compact=ultra`
+      )
+      .subscribe((res: { converted: string; amount: number }) => {
+        this.converted_amount = (amount * res[Object.keys(res)[0]]).toString();
+        this.converted_cur = converted_cur;
+        this.converted_symbol =
+          this.available_currencies.results[converted_cur].currencySymbol;
+      });
   }
 }
